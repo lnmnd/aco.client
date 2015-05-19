f75ae2a07afdbe52ee052eddc3e1dbe2beeb44c8
lnmnd <lnmnd@users.noreply.github.com>
2015-05-19 15:53:30 +0200

    import

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/aco.client.jar clojure.main -m aco.client.server
---
+++ env/prod/cljs/aco/client/prod.cljs
@@ -0,0 +1,7 @@
+(ns aco.client.prod
+  (:require [aco.client.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
