d3e9dca014a58c65050cc8637948a2ef1ff8fb66
lnmnd <lnmnd@users.noreply.github.com>
2015-05-23 19:46:30 +0200

    add bootstrap css

--- src/clj/aco/client/handler.clj
+++ src/clj/aco/client/handler.clj
@@ -14,9 +14,12 @@
      [:meta {:charset "utf-8"}]
      [:meta {:name "viewport"
              :content "width=device-width, initial-scale=1"}]
+     (include-css (if (env :dev) "css/bootstrap.css" "css/bootstrap.min.css"))
+     (include-css (if (env :dev) "css/bootstrap-theme.css" "css/bootstrap-theme.min.css"))
      (include-css (if (env :dev) "css/site.css" "css/site.min.css"))]
     [:body
-     [:div#app]
+     [:div.container
+      [:div#app]]
      (include-js "js/app.js")]]))
 
 (defroutes routes
