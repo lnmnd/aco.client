e4b148fbd91360362ac9fb6f150a668f996dd594
lnmnd <lnmnd@users.noreply.github.com>
2015-05-28 13:37:26 +0200

    show selection

--- src/cljs/aco/single/views.cljs
+++ src/cljs/aco/single/views.cljs
@@ -1,6 +1,12 @@
 (ns aco.single.views
   (:require [re-frame.core :as re-frame :refer [subscribe dispatch]]))
 
+(defn- find-by-url [url articles]
+  (reduce (fn [article found]
+            (if (= url (:url article))
+              article
+              found)) articles))
+
 (defn single-page []
   (let [loading (subscribe [:single/loading])
         error-loading (subscribe [:single/error-loading])
@@ -18,10 +24,16 @@
           [:p (take 10 (:date (:date @aco)))]
           [:p (:description @aco)]
           [:ul (for [article (:articles @aco)]
-              ^{:key (:url article)}
-              [:li [:a {:href "#" :on-click #(dispatch [:single/set-selected-article (:url article)])}
-                    (:title article)]])]
-          @selected-article])
+                 ^{:key (:url article)}
+                 [:li [:a {:href "#" :on-click #(dispatch [:single/set-selected-article (:url article)])}
+                       (if (= @selected-article (:url article))
+                         [:strong (:title article)]
+                         (:title article))]])]
+          (when @selected-article
+            (let [article (find-by-url @selected-article (:articles @aco))]
+              [:div
+               [:h2 (:title article)]
+               [:pre (:url article)]]))])
        [:p [:a {:href "#"
                 :on-click #(do (dispatch [:index/request-acos])
                                (dispatch [:set-active-panel :index]))}
