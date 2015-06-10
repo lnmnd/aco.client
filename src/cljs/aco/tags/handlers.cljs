3b96768c72efc3d529cb7ec3477ceb7560bca91b
lnmnd <lnmnd@users.noreply.github.com>
2015-06-10 11:29:52 +0200

    process tags

--- src/cljs/aco/tags/handlers.cljs
+++ src/cljs/aco/tags/handlers.cljs
@@ -5,6 +5,27 @@
 (register-handler
  :tags/request-tags
  (fn [db]
+   (GET
+    (str "http://localhost:8080/api/tags")
+    {:response-format :json
+     :keywords? true
+     :handler #(dispatch [:tags/process-tags %])
+     :error-handler #(dispatch [:tags/process-error %])})
    (-> db
        (assoc-in [:tags :error-loading] false)
        (assoc-in [:tags :loading] true))))
+
+
+(register-handler
+ :tags/process-tags
+ (fn [db [_ res]]
+   (-> db
+       (assoc-in [:tags :loading] false)
+       (assoc-in [:tags :tags] res))))
+
+(register-handler
+ :tags/process-error
+ (fn [db [_ res]]
+   (-> db
+       (assoc-in [:tags :loading] false)
+       (assoc-in [:tags :error-loading] true))))
