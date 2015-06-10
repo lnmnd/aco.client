c4663668c1c4363e392e62557c12f8f3460d44cc
lnmnd <lnmnd@users.noreply.github.com>
2015-06-10 11:20:20 +0200

    init db

--- src/cljs/aco/client/db.cljs
+++ src/cljs/aco/client/db.cljs
@@ -1,6 +1,8 @@
 (ns aco.client.db
-  (:require [aco.index.db :as index]))
+  (:require [aco.index.db :as index]
+            [aco.tags.db :as tags]))
 
 (def default-value
   {:active-panel :index
-   :index index/default-value})
+   :index index/default-value
+   :tags tags/default-valuep})
